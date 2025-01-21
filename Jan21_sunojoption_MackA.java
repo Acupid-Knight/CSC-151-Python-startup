c2dd5d54bca2ec489bfcd4791647ff87959fc5c0
Acupid-Knight <macka2859@student.faytechcc.edu>
2025-01-21 14:22:52 -0500

    Add files via upload

--- Jan21_sunojoption_MackA.java
+++ Jan21_sunojoption_MackA.java
@@ -23,6 +23,36 @@ public class Jan21_sunojoption_MackA {
        // reason is, I want to try stuff in suno and automating things is easier
        String firstGenre = JOptionPane.showInputDialog(null, "What's one song genre?");   
        String secondGenre = JOptionPane.showInputDialog(null, "What's another song genre?"); 
-    
+      
+       
+       
+       
+       
+       
+       String mix = firstGenre +"," + secondGenre;  
+       // String mix = secondGenre + firstGenre; // would be the other way
+       // TODO: add a mic icon instead
+       JOptionPane.showMessageDialog(null,mix, "here's your custom mix", JOptionPane.INFORMATION_MESSAGE);
+       // Can we make this less typing? Yes! we'll do it later
+       //tell(mix);
+   
     }
-}
+
+    public static String ask(String question) {
+        return JOptionPane.showInputDialog(null, question);
+    }
+   
+    public static void tell(String mix) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}   
+    
+    
+    
+    
+    
+    
+    
+    
+
