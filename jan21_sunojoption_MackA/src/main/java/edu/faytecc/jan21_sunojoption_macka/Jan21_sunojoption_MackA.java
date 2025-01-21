175eef2ffb694ee812b1a9285d67205b19ba867e
Acupid-Knight <macka2859@student.faytechcc.edu>
2025-01-21 13:50:30 -0500

    Add files via upload

---
+++ jan21_sunojoption_MackA/src/main/java/edu/faytecc/jan21_sunojoption_macka/Jan21_sunojoption_MackA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.faytecc.jan21_sunojoption_macka;
+
+/**
+ *
+ * @author macka2859
+ */
+public class Jan21_sunojoption_MackA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
