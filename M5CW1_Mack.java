9edb5918f8fe4e67aef9b6a0dffec576abbc268c
Acupid-Knight <macka2859@student.faytechcc.edu>
2025-03-28 15:12:38 +0000

    M5CW1_Mack

---
+++ M5CW1_Mack.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class M5CW1_Mack {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+
+        // Use a for loop to add numbers from 1 to 5
+        for (int i = 1; i <= 5; i++) {
+            sum += i; // Add the current number to sum
+        }
+
+        // Output the result
+        System.out.println("The sum of the numbers is: " + sum);
+    }
+}
+
